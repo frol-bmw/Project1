a4f3488e4e223e7f6730b89b41deb9c96b2cbcf0
Kosnanin <morozovkon050706@icloud.com>
2024-11-06 23:32:56 +0800

    Update test.cpp
    небольшая часть функций

--- test.cpp
+++ test.cpp
@@ -2,5 +2,17 @@
 
 int main()
 {
+using namespace std;
+  void displayMenu() {
+    cout << "Записная книжка" << endl;
+    cout << "1. Добавить запись" << endl;
+    cout << "2. Просмотреть записи" << endl;
+    cout << "3. Изменить запись" << endl;
+    cout << "4. Удалить запись" << endl;
+    cout << "5. Сохранить в файл" << endl;
+    cout << "6. Загрузить из файла" << endl;
+    cout << "7. Выход" << endl;
+ 
+}
 
 }
